42c5352e6689313ce07afae3d0017b3eea7651c6
kaaazu <wato9902@gmail.com>
2020-08-16 20:34:29 +0900

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,23 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::process;
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Config { query, filename }
-    }
-}
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // 引数解析時に問題
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
-    let mut f = File::open(config.filename).expect("file not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+        process::exit(1);
+    } 
 }
+
